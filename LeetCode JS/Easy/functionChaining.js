4d5d016c86884cfb9c2a554f958b1bd065e7a8e9
sheikhkaheel <sheikhkaheel@gmail.com>
2024-07-19 20:43:05 +0530

    modified functionChaining

--- LeetCode JS/Easy/functionChaining.js
+++ LeetCode JS/Easy/functionChaining.js
@@ -5,18 +5,18 @@ var expect = function(val){
                 return true;
             } 
             throw new Error("Not Equal");
+
         },
         notToBe : (num)=>{
             if(num !== val) {
                 return true;
-            } else {
-                throw new Error("Equal");
-            }
+            } 
+            throw new Error("Equal");
         }
     }
 }
 
 // let result = expect(5).toBe(5);
-let result = expect(5).toBe(null);
-// let result = expect(5).notToBe(null);
+// let result = expect(5).toBe(null);  // Throw new Error("Not Equal");
+let result = expect(5).notToBe(null);
 console.log(result);
