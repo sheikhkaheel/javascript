bf5939ec7567a0119e6320d9643ea42a1dede3d8
sheikhkaheel <sheikhkaheel@gmail.com>
2024-07-24 21:52:04 +0530

    added commit to test.js

--- InterviewQns/this.js
+++ InterviewQns/this.js
@@ -2,11 +2,11 @@ let objWithFn = {
     name: "kaheel",
     func: function () {
         var self = this;
-        console.log("Output of OuterFuntion for this.name = ", this.name);
-        console.log("Output of OuterFuntion for self.name = ", self.name);
+        console.log("Output of OuterFuntion for this.name = ", this.name);  // it point towards obj
+        console.log("Output of OuterFuntion for self.name = ", self.name);  // this also points towards obj
         (function () {
-            console.log("Output of Inner Function for this.name = ", this.name);
-            console.log("Output of Inner Function for self.name = ", self.name);
+            console.log("Output of Inner Function for this.name = ", this.name); // it points towards global obj
+            console.log("Output of Inner Function for self.name = ", self.name); // it points towards obj
         }());
     }
 }
